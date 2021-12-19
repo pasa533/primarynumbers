d509053d6db54b96cf184c2044a1da89e095a234
pasa533 <94807289+pasa533@users.noreply.github.com>
2021-12-19 20:35:33 +0100

    oguzhan kose
    oguzhan kose

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("How many numbers do you want to enter:");
+            int numbers = int.Parse(Console.ReadLine());
+
+            int j;
+            bool current = false;
+
+
+            for (int i = 2; i <= numbers; i++)
+            {
+
+                for (j = 2; j < i; j++)
+                {
+
+                    if (i % j == 0)
+                    {
+                        current = true;
+
+                    }
+
+                }
+
+                if (current == false)
+                {
+
+                    Console.WriteLine("Enter Number: {0} ", j);
+
+                }
+                else current = false;
+
+            }
+
+        }
+    }
+}
